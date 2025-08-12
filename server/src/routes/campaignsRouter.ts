873dafc3d9d52a1a47c92876b1765548a1f00358
Myron R Schippers Jr <dubtwee@gmail.com>
2025-08-12 16:32:03 -0400

    adding a GET route for the API that returns campaign KPI for days of the week

--- server/src/routes/campaignsRouter.ts
+++ server/src/routes/campaignsRouter.ts
@@ -4,6 +4,14 @@ import DB from '../db_connect';
 
 const router: express.Router = express.Router();
 
+type CampaignKpiAsDayOfWeek = {
+  id: number;
+  name: string;
+  description: string;
+  day_of_week_number: number;
+  total_hours_for_campaign: number;
+};
+
 /**
  * GET All Campaigns from the `campaign` table
  */
@@ -19,6 +27,61 @@ router.get(
 
         res.status(200).json({
           campaigns: rows,
+        });
+      });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: { message: 'Internal Server Error', info: error } });
+    }
+  }
+);
+
+/**
+ * GET Campaign KPIs grouped by days of the week to show what high volume days may be
+ */
+router.get(
+  '/:campaignId/kpi/day-of-week',
+  (req: Request, res: Response, _next: express.NextFunction): void => {
+    try {
+      const { campaignId } = req.params;
+      const query = `
+        SELECT
+          camp.id,
+          camp.name,
+          camp.description,
+          CAST(STRFTIME('%w', kpi.date) AS INTEGER) AS day_of_week_number,
+          SUM(kpi.hours) AS total_hours
+        FROM campaign AS camp
+        JOIN campaign_kpi AS kpi
+          ON camp.id = kpi.campaign_id
+        WHERE
+          camp.id = ?
+        GROUP BY
+          day_of_week_number`;
+      DB.all<CampaignKpiAsDayOfWeek>(query, [campaignId], (err, rows) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+          return;
+        }
+
+        const daysOfTheWeek = [
+          'Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday',
+        ];
+
+        res.status(200).json({
+          campaign_kpi_days: rows.map((kpiItem) => {
+            return {
+              ...kpiItem,
+              day_of_week: daysOfTheWeek[kpiItem.day_of_week_number],
+            };
+          }),
         });
       });
     } catch (error) {
