26f46a711e741e329205123f843eeee18291d0a9
Myron R Schippers Jr <dubtwee@gmail.com>
2025-08-12 22:32:36 -0400

    adding types for Campaign KPI day of the week

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -6,3 +6,16 @@ export type Agent = {
   created_at: string;
   is_active: boolean;
 };
+
+export type CampaignKpiDayOfWeek = {
+  id: number;
+  name: string;
+  description: string;
+  day_of_week_number: number;
+  total_hours: number;
+  day_of_week: string;
+};
+
+export type DayOfWeekResp = {
+  campaign_kpi_days: CampaignKpiDayOfWeek[];
+};
